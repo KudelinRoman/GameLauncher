f0c957f1a794370f8fbbb4af5e70a0916545816b
Roman Kudelin <roman.kudelin.00@gmail.com>
2021-05-29 00:46:22 +0300

    Доработка панели администрирования и ее логики. Добавление комментариев к классам: AdminPanel. GlobalParam. Добавление новых переменных в GlobalParam и доработка методов сохранения и считывания переменных в файлах

--- GameLauncher/AdminPanel.xaml.cs
+++ GameLauncher/AdminPanel.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
 namespace GameLauncher
 {
 	/// <summary>
@@ -19,9 +7,68 @@ namespace GameLauncher
 	/// </summary>
 	public partial class AdminPanel : Window
 	{
+		/// <summary>
+		/// Конструктор класса, в котором из GlobalParam устанавливаются значения в объекты на форме
+		/// </summary>
 		public AdminPanel()
 		{
 			InitializeComponent();
+			if (GlobalParam.Shell == true)
+			{
+				ThisProgShell.IsChecked = true;
+			}
+			else
+			{
+				WindowsShell.IsChecked = true;
+			}
+			Pass.Password = GlobalParam.Password;
+			dublePass.Password = GlobalParam.Password;
+			hoursStart.Text = GlobalParam.MassTime[0, 0].ToString();
+			hoursEnd.Text = GlobalParam.MassTime[1, 0].ToString();
+			minuteStart.Text = GlobalParam.MassTime[0, 1].ToString();
+			minuteEnd.Text = GlobalParam.MassTime[1, 1].ToString();
+		}
+		/// <summary>
+		/// Метод обрабатывающий событие клик, по кнопку "Сохранить", проверяет правильность заполнения полей и записывает их значение в GlobalParam
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			if (Pass.Password!= dublePass.Password)
+			{
+				MessageBox.Show("Пароли не совпадают!");
+				return;
+			}
+			else
+			{
+				int[,] masTime = new int[2,2];
+				try
+				{
+					masTime[0, 0] = Convert.ToInt32(hoursStart.Text);
+					masTime[0, 1] = Convert.ToInt32(minuteStart.Text);
+					masTime[1, 0] = Convert.ToInt32(hoursEnd.Text);
+					masTime[1, 1] = Convert.ToInt32(minuteEnd.Text);
+					GlobalParam.MassTime = masTime;
+				}
+				catch (Exception)
+				{
+					MessageBox.Show("Ошибка в задании времени");
+					return;
+				}
+				if (ThisProgShell.IsChecked == true)
+				{
+					GlobalParam.Shell = true;
+				}
+				else
+				{
+					GlobalParam.Shell = false;
+				}
+				GlobalParam.Password = Pass.Password;
+				GlobalParam.SaveList();
+
+				this.Close();
+			}
 		}
 	}
 }
